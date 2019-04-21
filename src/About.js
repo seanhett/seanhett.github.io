7f906e62ba113324805cb72dd128da61991aed74
root <zoombiniss@gmail.com>
2019-04-21 13:19:30 -0500

    Initial Commit

---
+++ src/About.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+ 
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>ABOUT ME</h2>
+        <p>My name is here.</p>
+ 
+        <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+      </div>
+    );
+  }
+}
+ 
+export default Home;
---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+ 
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>HELLO</h2>
+        <p>Welcome to my page</p>
+        <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+      </div>
+    );
+  }
+}
+ 
+export default Home;
